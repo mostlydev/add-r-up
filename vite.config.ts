f91f3c53c358fa939ac0a71d03d52b8297265ddf
Wojtek <wojtek@grabski.ca>
2026-03-25 20:50:03 -0400

    build: output to docs/, set base path for GitHub Pages from master

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/add-r-up/',
   publicDir: 'www',
   build: {
-    outDir: 'www',
-    emptyOutDir: false,
+    outDir: 'docs',
   },
 })
